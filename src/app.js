cc69a08eacafd9f4b854fcd0cacb760b3974f412
Srithar810 <srithar.s810@gmail.com>
2025-06-10 19:04:05 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -1,17 +1,30 @@
 const express = require("express");
 const app = express();
 
+app.get("/test", (req, res) => {
+  res.send("get method sending sucessfully");
+});
 
-app.get("/test",(req,res)=>{
-    res.send("get method sending sucessfully")
-})
-
-app.post("/test",(req,res)=>{
-    res.send("post method sending sucessfully")
-})
-
+app.post("/test", (req, res) => {
+  res.send("post method sending sucessfully");
+});
 
 
-app.listen(3000,()=>{
-    console.log("Server is running on 3000 port...");
+//------next() useage in route
+app.use(
+  "/us*er",
+  (req, res,next) => {
+    console.log("Handling the route user! ");
+    next();
+    res.send("Response1....!!!!");
+    
+  },
+  (req, res) => {
+    console.log("Handling the route user1....! ");
+    res.send("Response2....!!!!");
+  }
+);
+
+app.listen(3000, () => {
+  console.log("Server is running on 3000 port...");
 });
